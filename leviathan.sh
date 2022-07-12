54c65aa767aa58379a9d0941bf9cd7005e9847ed
catow <46374809+CatOw@users.noreply.github.com>
2022-07-12 17:25:35 +0300

    Create leviathan.sh

---
+++ leviathan.sh
@@ -0,0 +1,40 @@
+# level 0
+ssh leviathan.labs.overthewire.org -p 2223 -l leviathan0
+password is leviathan0
+ls -a
+ls .backup
+cat .backup/bookmarks.html | grep password
+
+# level 0 -> 1
+ssh leviathan.labs.overthewire.org -p 2223 -l leviathan1
+password is rioGegei8m
+ls -la
+file check
+cat check
+./check
+sex secret love god
+
+
+# level 1 -> 2
+ssh leviathan.labs.overthewire.org -p 2223 -l leviathan2
+password is 
+
+# level 2 -> 3
+ssh leviathan.labs.overthewire.org -p 2223 -l leviathan3
+password is 
+
+# level 3 -> 4
+ssh leviathan.labs.overthewire.org -p 2223 -l leviathan4
+password is 
+
+# level 4 -> 5
+ssh leviathan.labs.overthewire.org -p 2223 -l leviathan5
+password is 
+
+# level 5 -> 6
+ssh leviathan.labs.overthewire.org -p 2223 -l leviathan6
+password is 
+
+# level 6 -> 7
+ssh leviathan.labs.overthewire.org -p 2223 -l leviathan7
+password is 
